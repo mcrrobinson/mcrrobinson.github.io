a771bc36eb0679ca19ed7e723a103146a56edd29
Matt Robinson <mrmcrrobinson@gmail.com>
2020-06-06 18:53:36 +0100

    [Added] - Water gif, instead for faster loading.
    [Modified]
    - Changed method of rendering water.
    
    [Removed]
    - Removed image water.

--- js/fullpage.js
+++ js/fullpage.js
@@ -3608,7 +3608,6 @@
     * Shows a message in the console of the given type.
     */
     function showError(type, text){
-        window.console && window.console[type] && window.console[type]('fullPage: ' + text);
     }
 
     /**
