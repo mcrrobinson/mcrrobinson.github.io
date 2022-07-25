23ed2bc882b573dc3b36e5fd7a244cb54f3b099b
UP2022742 <mrmcrrobinson@gmail.com>
2022-07-25 10:29:04 +0100

    Changed to docker-compose setup.

--- WebServer/main.go
+++ WebServer/main.go
@@ -8,14 +8,12 @@ import (
 // main func
 func main() {
 	// Declare the API port.
-
 	projectTitle := flag.String("title", "TemplateApplication", "This will contain the title of the client.")
 	webPort := flag.Int("web-port", 443, "The Port is required to start the server.")
 	webIP := flag.String("web-ip", "localhost", "The IP is required to start the server.")
-	certificate := flag.String("cert", "certs/certificate.crt", "Location of the certificate.")
-	key := flag.String("key", "certs/private.key", "Location of th key.")
+	certificate := flag.String("cert", "/certs/fullchain.pem", "Location of the certificate.")
+	key := flag.String("key", "/certs/privkey.pem", "Location of th key.")
 
-	
 	// Make a bool to tell the thread when to stop.
 	stop := make(chan bool)
 
--- WebServer/routing/router.go
+++ WebServer/routing/router.go
@@ -22,7 +22,7 @@ type Router struct {
 
 // NewRouterStructure Defines an instance of the worker structure.
 func NewRouterStructure(title string, webPort int, webIP string, certFile string, keyFile string) *Router {
-	cert, _ := tls.LoadX509KeyPair("/etc/letsencrypt/live/mcrrobinson.com/fullchain.pem", "/etc/letsencrypt/live/mcrrobinson.com/privkey.pem")
+	cert, _ := tls.LoadX509KeyPair("/certs/fullchain.pem", "/certs/privkey.pem")
 	route := &Router{
 		logger:       log.New("module", "Router"),
 		apiServer:    nil,
